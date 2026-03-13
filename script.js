a4bd6f118c6850e9efd0ff1c5becfafde6b5bff3
KaranKaran00 <kharakaran504@gmail.com>
2026-03-13 20:57:14 +0530

    settimerout

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ document.getElementById("character");
 var block = document.getElementById("block");
 function jump(){
     character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },500);
 }
