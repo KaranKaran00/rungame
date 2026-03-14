ba4e9734a50145e4e6f42c355c84960de507166e
KaranKaran00 <kharakaran504@gmail.com>
2026-03-14 21:24:02 +0530

    checkDead

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ function jump(){
         character.classList.remove("animate");
     },500);
 }
+
+var checkDead = setInterval(function(){
+    var characterTop =
+    parseInt(window.getComputedStyle(character).
+    getPropertyValue("top"));
+    var blockleft =
+    parseInt(window.getComputedStyle(character).
+    getPropertyValue("left")); 
+},10);
